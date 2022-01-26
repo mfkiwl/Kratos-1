77a781f66a2d41783661f7f0964f338b28f520ce
Alejandro Cornejo Velázquez <alejandro.cornejo23@gmail.com>
2022-01-26 11:22:06 +0100

    .

--- applications/PfemFluidDynamicsApplication/python_scripts/pfem_fluid_dynamics_analysis.py
+++ applications/PfemFluidDynamicsApplication/python_scripts/pfem_fluid_dynamics_analysis.py
@@ -166,12 +166,12 @@ class PfemFluidDynamicsAnalysis(AnalysisStage):
         self.end_time   = self.project_parameters["problem_data"]["end_time"].GetDouble()
         self.delta_time = self.project_parameters["solver_settings"]["time_stepping"]["time_step"].GetDouble()
 
-        self.wave_process_wg8  = KratosPfemFluid.CalculateWaveHeightProcess(self.computing_model_part, 1, 0,  60.0,   0.0, 0.2,  "WG_08",   0.1)
-        self.wave_process_wg13 = KratosPfemFluid.CalculateWaveHeightProcess(self.computing_model_part, 1, 0, 165.3,   0.0, 0.2,  "WG_13",   0.1)
-        self.wave_process_wg14 = KratosPfemFluid.CalculateWaveHeightProcess(self.computing_model_part, 1, 0, 170.0,   0.0, 0.2,  "WG_14",   0.1)
-        self.wave_process_wg15 = KratosPfemFluid.CalculateWaveHeightProcess(self.computing_model_part, 1, 0, 223.5,   0.0, 0.025, "WG_15", 0.1)
-        self.wave_process_us4  = KratosPfemFluid.CalculateWaveHeightProcess(self.computing_model_part, 1, 0, 239.7,   0.0, 0.025, "US_4",  0.05)
-        self.wave_process_us5  = KratosPfemFluid.CalculateWaveHeightProcess(self.computing_model_part, 1, 0, 241  ,   0.0, 0.025, "US_5",  0.05)
+        self.wave_process_wg8  = KratosPfemFluid.CalculateWaveHeightProcess(self.computing_model_part, 1, 0,  60.0,   0.0, 0.3,  "WG_08",   0.05)
+        self.wave_process_wg13 = KratosPfemFluid.CalculateWaveHeightProcess(self.computing_model_part, 1, 0, 165.3,   0.0, 0.3,  "WG_13",   0.05)
+        self.wave_process_wg14 = KratosPfemFluid.CalculateWaveHeightProcess(self.computing_model_part, 1, 0, 170.0,   0.0, 0.3,  "WG_14",   0.05)
+        self.wave_process_wg15 = KratosPfemFluid.CalculateWaveHeightProcess(self.computing_model_part, 1, 0, 223.5,   0.0, 0.1,  "WG_15",   0.05)
+        self.wave_process_us4  = KratosPfemFluid.CalculateWaveHeightProcess(self.computing_model_part, 1, 0, 239.7,   0.0, 0.1,  "US_4",    0.025)
+        self.wave_process_us5  = KratosPfemFluid.CalculateWaveHeightProcess(self.computing_model_part, 1, 0, 241  ,   0.0, 0.1,  "US_5",    0.025)
 
     def InitializeSolutionStep(self):
         """This function performs all the required operations that should be executed
